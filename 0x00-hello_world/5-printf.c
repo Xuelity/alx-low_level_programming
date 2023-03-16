6f963a78253d1714f3825121cd98148d6d9ae7ea
Bernadette <bernadettehara@gmail.com>
2023-03-16 12:19:38 -0700

    printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,8 +6,8 @@
  */
 int main(void)
 {
-printf("\"with proper grammar, but the outcome is a piece of art,");
-
-/*return always (0) */
-return (0);
+	printf("\"with proper grammar, but the outcome is a piece of art,");
+	
+	/*return (0)*/
+	return (0)
 }
