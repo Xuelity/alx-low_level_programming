925f7a598a7b9b9c9746432e45d02e3463a8d251
Bernadette <bernadettehara@gmail.com>
2023-03-18 04:45:29 -0700

    print number

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * print all single digits numbers using putchar
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+		putchar(x);
+	putchar('\n');
+	/*return 0*/
+	return (0);
+}
