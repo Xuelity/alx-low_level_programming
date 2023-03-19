874e0cfd098d6693b35a41a135f2c1012cd7e386
Bernadette <bernadettehara@gmail.com>
2023-03-19 09:38:44 -0700

    print base

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * print numbers in hexadecimal
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x  <= 16; x++)
+		putchar("%x");
+	/*return value 0*/
+	return (0);
+}
