7e9270885da45b130a72184bbec2467bcf7a9cad
Bernadette <bernadettehara@gmail.com>
2023-03-21 07:24:43 -0700

    print putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * print .putchar and new line
+ * Return: Always 0 (succes)
+ */
+
+int main(void)
+{
+	putchar('.');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	/*return 0?*/
+	return (0);
+}
