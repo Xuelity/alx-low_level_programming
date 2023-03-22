e6ced0935bc92ad4686bc8183f55ebf5bd53f72e
Bernadette <bernadettehara@gmail.com>
2023-03-22 08:23:37 -0700

    print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <main.h>
+
+/**
+ * main - entry point
+ * print alphabet function
+ * 
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+		putchar(x);
+	putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/1.main.c
@@ -0,0 +1,13 @@
+#include <main.h>
+
+/**
+ * main - entry point
+ * check code
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
