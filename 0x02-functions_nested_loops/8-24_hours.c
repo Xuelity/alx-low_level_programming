aff360d32540a06ce2ac1fe00b2082e9889ee31f
Bernadette <bernadettehara@gmail.com>
2023-03-22 15:22:50 -0700

    minutes

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,7 +10,7 @@ void jack_bauer(void)
 {
 	int a, b, c, d;
 
-	for (a = 0; a < 3; a++)
+	for (a = 0; a <= 2; a++)
 	{
 		for (b = 0; b <= 9; b++)
 		{
@@ -18,7 +18,7 @@ void jack_bauer(void)
 			{
 				for (d = 0; d <= 9; d++)
 				{
-					if ((a > 2 && b < 4) || (c > 5 && d >= 9))
+					if (a >= 2 && b >= 3)
 						break;
 					_putchar(a + 48);
 					_putchar(b + 48);
