70e157d041dcc8ac6eb42ce12999bfb518723627
Bernadette <bernadettehara@gmail.com>
2023-03-22 16:08:39 -0700

    multiplication table

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add -add two integers
+ * @a: a is an integer
+ * @b:b is an interger values
+ *
+ * Return: integer
+ */
+
+int add(int a, int b)
+{
+	int x;
+
+	x = a + b;
+	_putchar(x + 48);
+	return (x);
+}
