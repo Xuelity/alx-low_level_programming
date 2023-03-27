fb921fb73e6d1bce94fcd995e52501f26ace5b10
Bernadette <bernadettehara@gmail.com>
2023-03-27 12:57:12 -0700

    prints string

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,21 +1,2 @@
 #include "main.h"
 #include <unistd.h>
-
-/**
- * _puts - prints string
- * @str: string
- * Return: void
- */
-
-void _puts(char *str)
-{
-	int i = 0;
-
-	while (str[i] != '\0')
-	{
-		_putchar(str[i]);
-		i++;
-	}
-
-	_putchar('\n');
-}
