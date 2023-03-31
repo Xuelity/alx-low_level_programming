1f62154a29bda078a1a158481a70a2f717043c88
Bernadette <bernadettehara@gmail.com>
2023-03-31 06:08:07 -0700

    print ten times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * more_numbers - print ten times numbers from 0-14
+ *
+ * return: no return
+ */
+
+void more_numbers(void)
+{
+	int x, c;
+
+	for (x = 0; x < 11; x++)
+	{
+		for (c = 0; c < 15; c++)
+		{
+			if (c > 9)
+			{
+				putchar((c / 10) + 48);
+				putchar((c % 10) + 48);
+			}
+		}
+		putchar('\n');
+	}
+
+}
