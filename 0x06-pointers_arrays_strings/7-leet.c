62a9b5357c7ed6ff4d94bcdd5453302935fa0dea
Bernadette <bernadettehara@gmail.com>
2023-04-03 12:25:15 -0700

    encoding a string

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * leet - encodes a string into 1337
+ * @s:s string arrays
+ * Return: string array
+ */
+
+char *leet(char *s)
+{
+	int x;
+
+	for (x = 0; s[x] != '\0'; x++)
+	{
+		if (s[x] == 'a' || s[x] == 'A')
+			s[x] = '4';
+		if (s[x] == 'e' || s[x] == 'E')
+			s[x] = '3';
+		if (s[x] == 'o' || s[x] == '0')
+			s[x] = '0';
+		if (s[x] =='t' || s[x] == 'T')
+			s[x] = '7';
+		if (s[x] == 'l' || s[x] == 'L')
+			s[x] = '1';
+	}
+
+	return (s);
+}
