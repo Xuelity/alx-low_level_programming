bf8218ee41b3a90f2791e52e76b5bcf7bdf0013b
Bernadette <bernadettehara@gmail.com>
2023-04-13 03:45:18 -0700

    function that checks if number is prime

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *  _sqrt_recursion - calculates the square root of a natural number
+ *  @n: is a natural number
+ *  Return: the square roort
+ */
+
+int _sqrt_recursion(int n)
+{
+
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * prime -checks if a number is prime with iterations
+ * @x: is an unsigned integer
+ * @y: is iterations
+ * Return: 1 if prime
+ */
+
+int prime(unsigned int x, int y)
+{
+	if (x % y == 0)
+	{
+		if (x == 0)
+			return (1);
+		else
+			return (0);
+	}
+	return (0 + prime(x, y + 1));
+}
+
+/**
+ * is_prime_number - checks if a number is prime
+ * @n: is an integer
+ * Return: 1 if prime otherwise 0
+ */
+
+int is_prime_number(int n)
+{
+	if (n == 0)
+		return (0);
+	if (n < 0)
+		return (0);
+	if (n == 1)
+		return (0);
+
+	return (prime(n, 2));
+}
